24c4638615098d92da83428498099f1d87537a80
queshav <keshav.saharia@gmail.com>
2014-04-22 08:57:28 -0700

    initial commit

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+gameover = False
+td = 5, tx = 3
+
+while not gameover:
+	# Print the board
+	print "   |   |   "
+	print "---+---+---"
+	print "   |   |   "
+	print "---+---+---"
+	print "   |   |   "
+	
+	row = input("Enter a row")
+	col = input("Enter a column")
+
