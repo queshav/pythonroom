3924441741e651127ec04bd002e839a37793589e
queshav <keshav.saharia@gmail.com>
2014-04-22 09:08:03 -0700

    pythonroom update

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,54 @@
 gameover = False
-td = 5, tx = 3
+tl = " "
+t = " "
+tr = " "
+l = " "
+c = " "
+r = " "
+bl = " "
+b = " "
+br = " "
+
+current = "X"
 
 while not gameover:
 	# Clear the output screen
 	print ":clear"
 	# Print the board
-	print "   |   |   "
+	print " " + tl + " | " + t + " | " + tr
 	print "---+---+---"
-	print "   |   |   "
+	print " " + l + " | " + c + " | " + r
 	print "---+---+---"
-	print "   |   |   "
+	print " " + bl + " | " + b + " | " + br
 	
-	row = input("Enter a row")
-	col = input("Enter a column")
+	row = 0
+	col = 0
+	while row < 1 or col < 1 or row > 3 or col > 3:	
+		row = int(input("Enter a row"))
+		col = int(input("Enter a column"))
+		if row == 5:
+			gameover = True
 	
-	if row == "5":
-		gameover = True
+	if row == 1 and col == 1:
+		tl = current
+	if row == 1 and col == 2:
+		t = current
+	if row == 1 and col == 3:
+		tr = current
+	if row == 2 and col == 1:
+		l = current
+	if row == 2 and col == 2:
+		c = current
+	if row == 2 and col == 3:
+		r = current
+	if row == 3 and col == 1:
+		bl = current
+	if row == 3 and col == 2:
+		b = current
+	if row == 3 and col == 3:
+		br = current
+	
+	if current == "X":
+		current = "O"
+	else:
+		current = "X"
